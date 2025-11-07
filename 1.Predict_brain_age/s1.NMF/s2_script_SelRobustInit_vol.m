0f9eed0c9ceb782952795d1273c7267eb6fe4f7b
ALICE1029 <50513038+ALICE1029@users.noreply.github.com>
2025-11-07 16:13:33 +0800

    Update s2_script_SelRobustInit_vol.m

--- 1.Predict_brain_age/s1.NMF/s2_script_SelRobustInit_vol.m
+++ 1.Predict_brain_age/s1.NMF/s2_script_SelRobustInit_vol.m
@@ -3,7 +3,7 @@ clear;
 %% write loading matrix
 %addpath(genpath('path-to-{CollaborativeBrainDecomposition}'));
 for i=18
-  candidateLstFile = strcat(('/HeLabData2/cxpang/CBIRD_FOR_DIDA_NGSR/result/init/fmri_volboostrap_comp'),num2str(i),'/init.txt');
+  candidateLstFile = strcat(('/HeLabData2/cxpang/CBIRD_FOR_DIDA_NGSR/result/init/fmri_volboostrap_comp'),num2str(i),'/init.txt'); % the path list for every bootstrap init.mat
   outDir =strcat( '/HeLabData2/cxpang/CBIRD_FOR_DIDA_NGSR/result/init_robust/',num2str(i));
    initV = selRobustInit(candidateLstFile,i,outDir,i);
 
