e0b7f7fe28d6ab26b92d9e1e58de7b2e7d8f4a97
ALICE1029 <50513038+ALICE1029@users.noreply.github.com>
2025-11-08 22:25:26 +0800

    Update s2_predict_mfd.m

--- 1.Predict_brain_age/s2.brain_age/s2_predict_mfd.m
+++ 1.Predict_brain_age/s2.brain_age/s2_predict_mfd.m
@@ -27,7 +27,7 @@ C_Range =1%power(10, -5:5);
 Weight_Flag = 0;
 Permutation_Flag = 0;
 randNet = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18];
-[Prediction] = pca_svr(Subjects_Data, Subjects_Scores,covariate,FoldQuantity, Pre_Method, C_Range, network_num,voxel_num,voxel_num,network_num,randNet,Weight_Flag, Permutation_Flag, ResultantFolder);
+[Prediction] = pca_svr(Subjects_Data, Subjects_Scores,covariate,FoldQuantity, Pre_Method, C_Range, ResultantFolder);
 %% all hc to mdd model
 ResultantFolder = [data_dir,'/SVR_all_no_regress_fold20_remove65sex_deletemfd'];
 load([ResultantFolder filesep 'Prediction.mat'], 'Prediction');
