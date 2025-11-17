1ea9a3e5f1434d004473126f173a7dd97f937b3f
ALICE1029 <50513038+ALICE1029@users.noreply.github.com>
2025-11-17 21:17:03 +0800

    Update step1_hamd_subscale_feature_generate.m

--- 3.Analyse_subtypes_clinical/step1_hamd_subscale_feature_generate.m
+++ 3.Analyse_subtypes_clinical/step1_hamd_subscale_feature_generate.m
@@ -1,9 +1,7 @@
 addpath(genpath('/home/cxpang/matlab/combat'))
 %%
-load dida
 voxel_num=45892;
 network_num=18;
-sbj_num=2227;
 method='NGSR';
 load('/home/cxpang/matlab/code/8.brain_age/using_mat/index_all_remove65sex.mat')
 load ('/HeLabData2/cxpang/DIDA/using_mat/glmvariable_remove65.mat')%age,group,mfd,sex
@@ -13,7 +11,7 @@ mdd=find(group==2);
 sub1_index=mdd(find(Idx==1));
 sub2_index=mdd(find(Idx==2));
 %% 
-mdd_hamd=importdata('/home/cxpang/matlab/code/4.HAMD_correlation/using_mat/mdd_hamd_num_sub.txt');
+mdd_hamd=importdata('/home/cxpang/matlab/code/4.HAMD_correlation/using_mat/mdd_hamd_num_sub.txt');% index of patients have hamd score
 sub1_hamd=intersect(mdd_hamd,sub1_index);
 load ('/HeLabData2/cxpang/DIDA/using_mat/glmvariable_remove65.mat')%age,group,mfd,sex
 age=age(sub1_hamd);
@@ -47,8 +45,6 @@ save('/home/cxpang/matlab/code/8.brain_age/using_mat/hamd_sub_sub2_remove65.mat'
 load ('/HeLabData2/cxpang/DIDA/using_mat/glmvariable.mat')
 new_index=find(age>=65);
 load(strcat('/HeLabData2/cxpang/DIDA/',method,'/result/before_combat/regress_variable.mat'))
-load('/HeLabData2/cxpang/DIDA/using_mat/pos_zero.mat','pos')
-x (pos,:) = [];
 x(:,new_index)=[];
 x=x(:,mdd_hamd);
 load(strcat('/home/cxpang/matlab/code/4.HAMD_correlation/using_mat/hamd_sub.mat'))
@@ -58,12 +54,11 @@ load ('/HeLabData2/cxpang/DIDA/using_mat/glmvariable_remove65.mat')%age,group,mf
 load ('/HeLabData2/cxpang/DIDA/using_mat/site_remove65.mat')%
 site=site(mdd_hamd);
 pos=find(all(x==0,2));% because combat can't accept variable that is same,so find the position that everyone's loading is zero
-%save(strcat('/HeLabData2/cxpang/DIDA/',method,'/hamd/loading_pos_sub.mat'),'pos')% to further analyse loading weight
+save(strcat('/HeLabData2/cxpang/DIDA/',method,'/hamd/loading_pos_sub.mat'),'pos')% to further analyse loading weight
 sex(find(sex==-1))=2;
 sex_d=dummyvar(sex);%dummy
 sex=sex_d(:,2);
 mod=[age(mdd_hamd) hamd sex(mdd_hamd) mfd(mdd_hamd)];% combat: hamd as protect variable
-x_tmp=x;
 x (pos,:) = [];
 data_harmonized = combat(x,(site),mod,1);%combat
 loading=data_harmonized';
@@ -75,62 +70,17 @@ loading_sub2=loading(sub2_hamd_index,:);
 save('/HeLabData2/cxpang/DIDA/NGSR/result_xy/hamd/loading_sub_sub1_remove65.mat','loading_sub1')
 save('/HeLabData2/cxpang/DIDA/NGSR/result_xy/hamd/loading_sub_sub2_remove65.mat','loading_sub2')
 
-%%
-%step2_pls_sub_topography.py
-%%
-%% step3 select feature for time saving
-ResultsFolder = '/HeLabData2/cxpang/DIDA/NGSR/result_xy/hamd';
-matFolder='/home/cxpang/matlab/code/8.brain_age/using_mat';
-
-fold=10
-FoldQuantity = 10;
-select_p_all=[0.05,0.01,0.001];
-for j=1:3
-select_p=select_p_all(j);
-ResultantFolder = strcat('/HeLabData2/cxpang/DIDA/NGSR/result_xy/brain_age/sub1/p',num2str(select_p))
-
-for i=1:fold
-    i
-    load(strcat('/HeLabData2/cxpang/DIDA/NGSR/result_xy/brain_age/sub1/Fold_',num2str(i-1),'_regress_remove65.mat'))
-    load(strcat('/HeLabData2/cxpang/DIDA/NGSR/result_xy/brain_age/sub1/Fold_',num2str(i-1),'_regress_score_remove65site.mat'))
-    [my_corr,p]=corr(Subjects_Data_train,Subjects_Score_train,'type','Pearson');
-    pos = find(any(p <select_p, 2));
-    pos=pos-1;    
-    save(strcat(ResultantFolder,'/fold_',num2str(i-1),'_mask_remove65site.mat'),'pos')
-end
-end
-%%
-ResultsFolder = '/HeLabData2/cxpang/DIDA/NGSR/result_xy/hamd';
-matFolder='/home/cxpang/matlab/code/8.brain_age/using_mat';
-
-fold=10;
-FoldQuantity = 10;
-select_p_all=[0.05,0.01,0.001];
-for j=1:3
-select_p=select_p_all(j);
-ResultantFolder = strcat('/HeLabData2/cxpang/DIDA/NGSR/result_xy/brain_age/sub2/p',num2str(select_p))
-
-for i=1:fold
-    i
-    load(strcat('/HeLabData2/cxpang/DIDA/NGSR/result_xy/brain_age/sub2/Fold_',num2str(i-1),'_regress_remove65.mat'))
-    load(strcat('/HeLabData2/cxpang/DIDA/NGSR/result_xy/brain_age/sub2/Fold_',num2str(i-1),'_regress_score_remove65site.mat'))
-    [my_corr,p]=corr(Subjects_Data_train,Subjects_Score_train,'type','Pearson');
-    pos = find(any(p <select_p, 2));
-    pos=pos-1;    
-    save(strcat(ResultantFolder,'/fold_',num2str(i-1),'_mask_remove65site.mat'),'pos')
-end
-end
-%% see the hamd subscale result
+%% step2_pls_sub_topography.py
+%% see the correlation result
 fold=10;
 for  k=1
     draw=cell(2,1);
     for j=0:fold-1
         load(strcat('/HeLabData2/cxpang/DIDA/',method,'/result_xy/hamd/cca/AtlasLoading/loading_sub_sub1_select0.001/Time_0/Fold_',num2str(j),'comp_uv.mat'))
-        
         tmp1=u(:,k);
         tmp2=v(:,k);
         draw{1}=[draw{1};tmp1];
-        draw{2}=[ draw{2};tmp2];
+        draw{2}=[draw{2};tmp2];
     end
     my_corr(k)=corr( draw{1}, draw{2});
 end
@@ -141,14 +91,10 @@ for k=1:1000
     k
     draw=cell(2,1);
     ResultantFolder = [strcat('/HeLabData2/cxpang/DIDA/',method,'/result_xy/hamd/cca/AtlasLoading/loading_sub_sub1_select0.001/Permutation/',num2str(k)),'/Time_0/'];
-    if(exist(strcat(ResultantFolder,'Fold_9comp_uv.mat')))
+ 
         for  s=1
              draw=cell(2,1);
             for i=0:9%to avoid the condition that predict value same,correlation nan
-                %       if(max(Prediction.Score{i})-min(Prediction.Score{i})==0)
-                %           Prediction.Corr(i)=0;
-                %       end
-                %
                 ResultantFolder = [strcat('/HeLabData2/cxpang/DIDA/',method,'/result_xy/hamd/cca/AtlasLoading/loading_sub_sub1_select0.001/Permutation/',num2str(k)),'/Time_0/'];
                 load(strcat(ResultantFolder,'Fold_',num2str(i),'comp_uv.mat'))
                 tmp1=u(:,s);
@@ -159,9 +105,7 @@ for k=1:1000
             
             corr_permut_all(k,s)=corr(draw{1},draw{2});
         end
-    else
-        bug=[bug,k];
-    end
+ 
 end
 for k=1
 p(k)=length(find(corr_permut_all(:,k)>=my_corr(k)))/1000;
